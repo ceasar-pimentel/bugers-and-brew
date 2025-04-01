add821ab42b7fad50776105ecdf56d52e050ba32
ceasar pimentel <51726470+ceasar-pimentel@users.noreply.github.com>
2025-04-01 16:08:05 -0700

    maintenance: setting up build process for github pages

--- diner/vite.config.js
+++ diner/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "/front-end-demo-projects/diner/", // Change for project2 and project3 accordingly
+	build: {
+		outDir: `../dist/diner`, // Change for project2 and project3
+	},
+});
