3751d49643d080b5c92c2acced1505fc79fa797e
ceasar pimentel <51726470+ceasar-pimentel@users.noreply.github.com>
2025-04-01 19:49:37 -0700

    experimenting with the base

--- diner/vite.config.js
+++ diner/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/front-end-demo-projects/diner/", // Change for project2 and project3 accordingly
+	base: "/diner/", // Change for project2 and project3 accordingly
 	build: {
-		outDir: `dist/diner`, // Change for project2 and project3
+		outDir: `../dist/diner`, // Change for project2 and project3
 	},
 });
